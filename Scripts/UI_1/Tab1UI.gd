6926753d66fade7dcaa68979f91a5c7baf076ecd
Rycer <szymkris5@gmail.com>
2023-09-30 12:11:13 +0200

    Organised things, added a fullscreen toggle, fixed viewport anchors

--- Scripts/UI_1/Tab1UI.gd
+++ Scripts/UI_1/Tab1UI.gd
@@ -1,5 +1,6 @@
 extends Control
 var resume_container = 0
+var fullscreen_val = 0
 func _ready():
 	$Player_exp_ProgBar.hide()
 	$Skills_container.hide()
@@ -19,3 +20,13 @@ func _on_learn_skill_prog_bar_skill_progbar_full():
 	resume_container += 1
 	if resume_container == 2:
 		$Resume_container.show()
+
+
+func _on_check_button_pressed():
+	fullscreen_val += 1
+	if fullscreen_val == 1:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
+	if fullscreen_val == 2:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		fullscreen_val = 0 
